180ac16c8ddf7324f69705ee9b9427dfa2a4f965
Sebastian Johnsson <sebastian@agiley.se>
2014-12-14 22:09:09 +0100

    Add support for supplying full comparisons parameter to text search.

--- lib/copyscape/text_search.rb
+++ lib/copyscape/text_search.rb
@@ -4,8 +4,8 @@ module Copyscape
   
   class TextSearch < RequestBase
 
-    def initialize(text, encoding = 'UTF-8')
-      http_response = post_response(:o => 'csearch', :t => text, :e => encoding)
+    def initialize(text, full_comparisons = 0, encoding = 'UTF-8')
+      http_response = post_response(:o => 'csearch', :t => text, :c => full_comparisons, :e => encoding)
       @response = Response.new(http_response)
     end
 
